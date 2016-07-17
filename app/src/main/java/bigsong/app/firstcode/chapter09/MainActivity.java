d989da6828ef7095676c3b0cfa6bdc471d485f1f
yischool611 <737393622@qq.com>
2016-07-17 10:20:21 +0800

    学习Service

--- app/src/main/java/bigsong/app/firstcode/chapter09/MainActivity.java
+++ app/src/main/java/bigsong/app/firstcode/chapter09/MainActivity.java
@@ -1,7 +1,10 @@
 package bigsong.app.firstcode.chapter09;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -11,13 +14,55 @@ import bigsong.app.firstcode.R;
  * Created by BigSong on 16/7/9.
  */
 public class MainActivity extends AppCompatActivity {
+
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter09_main);
     }
 
-    public void onCLick_androidTreadTest(View view){
-        startActivity(new Intent(MainActivity.this,AnroidThreaTestActivity.class));
+    public void onCLick_androidTreadTest(View view) {
+        startActivity(new Intent(MainActivity.this, AnroidThreaTestActivity.class));
     }
+
+    public void onClick_startService(View view) {
+        Intent startIntent = new Intent(this, MyService.class);
+        startService(startIntent);
+    }
+
+    public void onClick_stopService(View view) {
+        Intent stopIntent = new Intent(this, MyService.class);
+        stopService(stopIntent);
+    }
+
+    public void onClick_bindService(View view) {
+        Intent intent = new Intent(this, MyService.class);
+        bindService(intent, connection, BIND_AUTO_CREATE);
+    }
+
+    public void onClick_unbindService(View view) {
+        unbindService(connection);
+    }
+
+    public void onClick_startLongRunningService(View view){
+        Intent intent = new Intent(this,LongRunningService.class);
+        startService(intent);
+    }
+
 }
--- app/src/main/java/bigsong/app/firstcode/chapter09/MyService.java
+++ app/src/main/java/bigsong/app/firstcode/chapter09/MyService.java
@@ -1,32 +1,100 @@
 package bigsong.app.firstcode.chapter09;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+
+import bigsong.app.firstcode.*;
+import bigsong.app.firstcode.MainActivity;
 
 /**
  * Created by BigSong on 16/7/10.
  */
 public class MyService extends Service {
+
+    private DownloadBinder binder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d("MyService", "startDownload");
+        }
+
+        public int getProgress() {
+            Log.d("MyService", "getProgress");
+            return 0;
+        }
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return binder;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("MyService", "onCreate");
+       /* Notification notification = new Notification(R.mipmap.ic_launcher,"Notification comes",System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this, bigsong.app.firstcode.MainActivity.class);
+        PendingIntent pendingIntent= PendingIntent.getActivity(this,0,notificationIntent,0);
+        notification.*/
+       /* NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        notificationBuilder.setContentTitle("This is title");
+        notificationBuilder.setContentText("This is text");
+        notificationBuilder.setTicker("New message");
+        notificationBuilder.setAutoCancel(true);
+        notificationBuilder.setNumber(12);
+//        notificationBuilder.setDefaults(Notification.DEFAULT_ALL);
+        Intent resultIntent = new Intent(this, bigsong.app.firstcode.MainActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this,0,resultIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        notificationBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0,notificationBuilder.build());*/
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setTicker("This is Ticker");
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        mBuilder.setContentTitle("This is ContentTitle");
+        mBuilder.setContentText("This is ContentText");
+        mBuilder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.wtf));
+        mBuilder.setNumber(6);
+//        mBuilder.setOngoing(true);
+        mBuilder.setAutoCancel(true);
+        Intent resultIntent = new Intent(this, bigsong.app.firstcode.MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent pIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(pIntent);
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.notify(1, mBuilder.build());
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }).start();
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d("MyService", "onDestroy");
     }
 }
